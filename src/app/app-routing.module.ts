26967bd942906aa91560065ed7d438d2c3a6c207
MohamedAymanNeo <az.mohamedayman12@gmail.com>
2022-06-18 18:10:31 +0200

    update structure and add service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: ()=> import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: ()=> import('./components/auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: 'types',
-    loadChildren: ()=> import('./game-types/game-types.module').then(m => m.GameTypesModule)
+    loadChildren: ()=> import('./components/game-types/game-types.module').then(m => m.GameTypesModule)
   },
   {
     path: 'game',
-    loadChildren: ()=> import('./play-game/play-game.module').then(m => m.PlayGameModule)
+    loadChildren: ()=> import('./components/play-game/play-game.module').then(m => m.PlayGameModule)
   },
 ];
 
